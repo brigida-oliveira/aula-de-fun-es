4e6f672d313bbc8948db7d8bc5bf4f99366d4fa9
Brígida Oliveira <ocfb0914@hotmail.com>
2025-01-14 08:12:52 -0300

    Atualização do código

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soma(int x, int y){
+    return x+y;
+}
+
+
 int main()
 {
-    printf("Hello world!\n");
+    int a, b, resposta;
+    printf("Vamos somar! :D\n\n");
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%d", &a);
+
+    printf("Digite o segundo numero:\n");
+    scanf("%d", &b);
+
+    resposta = soma(a, b);
+
+    printf("Resultado: %d\n", resposta);
     return 0;
 }
